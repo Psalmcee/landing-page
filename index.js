d7e783a61936f60ff81fb979edc852a94b537df0
Psalmcee <100202765+Psalmcee@users.noreply.github.com>
2022-06-08 19:25:21 +0100

    added greeting to display accordingly

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const time = document.getElementById('time');
-const greeting = document.getElementById('greeting');
+const greet = document.getElementById('greet');
 const focus = document.getElementById('focus');
 const focusAnswer = document.getElementById('focus-answer'); 
+const background = document.getElementById('bg')
 let displayTime;
+let greeting;
 
 function showTime() {
     let today = new Date()
@@ -10,8 +12,9 @@ function showTime() {
     min = today.getMinutes(),
     sec = today.getSeconds();
 
-    //convert 24 hours into 12 hours
-    let amPm = hour % 12 || 12
+   
+    //convert 24 hours to 12 hours
+    let amPm = hour % 12 || 12;
 
     //add zeros in front of single integer
     amPm = (amPm < 10 ? '0' + amPm : amPm )
@@ -21,9 +24,27 @@ function showTime() {
     time.innerHTML = `${amPm}:${min}:${sec} `
     time.innerHTML += (hour >= 12 ? 'PM' : 'AM')
     
+    showGreeting()
+
+
     setTimeout(showTime, 1000)
 }
-showTime()
 
-//adding AM or PM
+showTime();
 
+//greeting to display current time && change picture according to time of the day.
+function showGreeting() {
+    greeting  = ['Good morning User', 'Good afternoon User', 'Good evening User'];
+
+    if (hour < 12) {
+        greet.innerHTML = greeting[0]
+        document.body.style.backgroundImage = "url('smileyrose.jpg)" 
+    } else 
+    if (hour >= 12 && hour < 18) {
+        greet.innerHTML = greeting[1]
+        document.body.style.backgroundImage = "url('flowers.jpg')"
+    } else {
+        greet.innerHTML = greeting[2]
+        document.body.style.backgroundImage = "url('thankyou.jpg')"
+    }
+};
